e531533bd70285b148c7ba4643b3394bf41e1f3e
pagh <pagh@paghsPC>
2015-08-26 09:24:00 +0200

    opg 1,2 & 3. Svar på opgave to ting: Da de to threads ikke (nødvendigvis) tilgår variablen samtidig kan der i sjældne cases opstå en situation hvor de prøver at manipulerer den samme variablen, men i stedet tilgår en "lokal" instans. Derved kan opstå en "umulig" fejl. Fejlen er dog ikke konstateret af nogen, hvorfor det er svært at vide at den egentlig kan hænde. (Jeg kørte programmet for fuld smæk (ingen prints eller noget) i ca 5 mins uden at den så et eneste ulige tal). OPG 3 er løst ved at køre ball i en seperat thread fra eventlisteren.

--- q1w1/src/q1_2w1/T1.java
+++ q1w1/src/q1_2w1/T1.java
@@ -11,25 +11,23 @@ package q1_2w1;
  */
 public class T1 extends Thread
 {
-    //Understående linje er for part to af opgaven
-    private static Object abekat = new Object();
+
     private Even even;
 
     public T1(Even even)
     {
         this.even = even;
     }
+
     @Override
     public void run()
     {
-        //Understående linje er for part to af opgaven
-        synchronized (abekat)
-        {
-            gogo(even);
-        }
+
+        gogo(even);
+
     }
 
-    public int gogo(Even even)
+    public synchronized int gogo(Even even)
     {
         return even.next();
     }
